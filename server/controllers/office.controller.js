2805ccfcae117d6ba14768c0c925735043ef9767
Niyitangasam <niyitangasam@gmail.com>
2019-04-17 21:19:06 +0200

    [feature 165408913] add results endpoints

--- server/controllers/office.controller.js
+++ server/controllers/office.controller.js
@@ -2,6 +2,16 @@ import OfficeModel from '../models/office.model';
 import Helper from '../helpers/helper';
 
 const officeController = {
+    getOfficesResult: async (req, res) => {
+        const getResultQuery = new OfficeModel(req.params.id);
+        if (!await getResultQuery.getResults()) {
+          return res.status(500).send({ status: 500, Error: 'Error in getting data' });
+        }
+        if (getResultQuery.result.length === 0) {
+          return res.status(404).send({ status: 404, Error: 'Offices not found' });
+        }
+        return res.status(200).send({ status: 200, data: getResultQuery.result });
+      },
     createNewOffice: async (req, res) => {
         const result = Helper.validateOffice(req.body);
         if (result.error) {
@@ -18,16 +28,7 @@ const officeController = {
         return res.status(201).send({ status: 201, message: 'New office created', data: newOffice.result });
     },
 
-   getOfficesResult: async (req, res) => {
-        const getResultQuery = new OfficeModel(req.params.id);
-        if (!await getResultQuery.getResults()) {
-          return res.status(500).send({ status: 500, Error: 'Error in getting data' });
-        }
-        if (getResultQuery.result.length === 0) {
-          return res.status(404).send({ status: 404, Error: 'Offices not found' });
-        }
-        return res.status(200).send({ status: 200, data: getResultQuery.result });
-      }
+ 
 };
 
 
