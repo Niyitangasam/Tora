ec9c13ce2ddea681efa1a9dce90fa7ed24c571af
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-17 16:06:11 +0200

    [#165407776] Add method to find all offices

--- server/models/office.model.js
+++ server/models/office.model.js
@@ -11,10 +11,22 @@ class Office {
     const {
         type, name,
     } = this.data;
+
     const values = [type, name];
 
     try {
       const { rows } = await dbCon.query('INSERT INTO offices(type, name) VALUES ($1, $2) returning name, type', values);
+      this.result = rows;
+      return true;
+    } catch (error) {
+      this.error = error;
+      return false;
+    }
+  }
+
+  async findAll() {
+    try {
+      const { rows } = await dbCon.query('SELECT * FROM offices', []);
       this.result = rows;
       return true;
     } catch (error) {
