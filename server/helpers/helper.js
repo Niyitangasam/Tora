9be7050093002c9bdb57773eb78fb108c4d525cc
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-18 14:15:50 +0200

    [#165442014] Beautify the codes

--- server/helpers/helper.js
+++ server/helpers/helper.js
@@ -4,63 +4,63 @@ import bcrypt from 'bcrypt';
 
 class Helper {
 
-    static validateVote(vote) {
-        const schema = Joi.object().keys({
-          createdBy: Joi.number().integer().required(),
-          office: Joi.number().integer().required(),
-          candidate: Joi.number().integer().required(),
-        });
-        return Joi.validate(vote, schema);
-      }
-      static validateUser(user) {
-        const schema = Joi.object().keys({
-          firstname: Joi.string().required(),
-          lastname: Joi.string().required(),
-          othername: Joi.string().required(),
-          email: Joi.string().trim().email({
-            minDomainAtoms: 2,
-        }).required(),
-          password: Joi.string().min(8).required(),
-          isadmin: Joi.boolean().required(),
-        });
-        return Joi.validate(user, schema);
-      }
-    static invalidDataMessage(res, result) {
-        const errors = [];
-        for (let index = 0; index < result.error.details.length; index += 1) {
-          errors.push(result.error.details[index].message.split('"').join(' '));
-        }
-        return res.status(422).send({ status: 422, Error: errors });
-      }
-      static generateToken(userinfo) {
-        const Issuetoken = jwt.sign(userinfo[0],
-          process.env.SECRET, { expiresIn: '1d' });
-      return Issuetoken;
-      }
-      static hashPassword(password) {
-        const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
-        return hashedPassword;
-      }
+  static validateVote(vote) {
+    const schema = Joi.object().keys({
+      createdBy: Joi.number().integer().required(),
+      office: Joi.number().integer().required(),
+      candidate: Joi.number().integer().required(),
+    });
+    return Joi.validate(vote, schema);
+  }
+  static validateUser(user) {
+    const schema = Joi.object().keys({
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      othername: Joi.string().required(),
+      email: Joi.string().trim().email({
+        minDomainAtoms: 2,
+      }).required(),
+      password: Joi.string().min(8).required(),
+      isadmin: Joi.boolean().required(),
+    });
+    return Joi.validate(user, schema);
+  }
+  static invalidDataMessage(res, result) {
+    const errors = [];
+    for (let index = 0; index < result.error.details.length; index += 1) {
+      errors.push(result.error.details[index].message.split('"').join(' '));
+    }
+    return res.status(422).send({ status: 422, Error: errors });
+  }
+  static generateToken(userinfo) {
+    const Issuetoken = jwt.sign(userinfo[0],
+      process.env.SECRET, { expiresIn: '1d' });
+    return Issuetoken;
+  }
+  static hashPassword(password) {
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+    return hashedPassword;
+  }
 
-    static name(input, required) {
-        if (!input && !required) {
-          return {
-            isValid: true,
-          };
-        }
-        if (input.match(/[a-z]{2}/i) && !input.match(/[0-9!$%*|}{:><?~`_&#^=]/)) {
-          return {
-            isValid: true,
-          };
-        }
-        return {
-          isValid: false,
-    
-          error: 'Please enter valid characters! Only alphabetic characters allowed.!!',
-    
-    
-        };
-      }
+  static name(input, required) {
+    if (!input && !required) {
+      return {
+        isValid: true,
+      };
+    }
+    if (input.match(/[a-z]{2}/i) && !input.match(/[0-9!$%*|}{:><?~`_&#^=]/)) {
+      return {
+        isValid: true,
+      };
+    }
+    return {
+      isValid: false,
+
+      error: 'Please enter valid characters! Only alphabetic characters allowed.!!',
+
+
+    };
+  }
 
   static validateOffice(office) {
     const officeKeys = Joi.object().keys({
@@ -70,29 +70,29 @@ class Helper {
 
     return Joi.validate(office, officeKeys);
   }
-  
-      static validateCandidate(candidate) {
-        const schema = Joi.object().keys({
-          officeId: Joi.number().integer().required(),
-          partyId: Joi.number().integer().required(),
-          userId: Joi.number().integer().required(),
-        });
-        return Joi.validate(candidate, schema);
-      }
-      static comparePassword(passwordHash, password) {
-      const comparedPassword = bcrypt.compareSync(password, passwordHash);
-      return comparedPassword;
-      } 
-      static validateLogin(user) {
-        const schema = Joi.object().keys({
-          email: Joi.string().trim().email({
-            minDomainAtoms: 2,
-        }).required(),
-          password: Joi.string().min(8).required(),
-        })
-    
-        return Joi.validate(user, schema);
-      }   
+
+  static validateCandidate(candidate) {
+    const schema = Joi.object().keys({
+      officeId: Joi.number().integer().required(),
+      partyId: Joi.number().integer().required(),
+      userId: Joi.number().integer().required(),
+    });
+    return Joi.validate(candidate, schema);
+  }
+  static comparePassword(passwordHash, password) {
+    const comparedPassword = bcrypt.compareSync(password, passwordHash);
+    return comparedPassword;
+  }
+  static validateLogin(user) {
+    const schema = Joi.object().keys({
+      email: Joi.string().trim().email({
+        minDomainAtoms: 2,
+      }).required(),
+      password: Joi.string().min(8).required(),
+    })
+
+    return Joi.validate(user, schema);
+  }
 
 }
 
