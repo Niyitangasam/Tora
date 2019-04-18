d35ffd04ad3b6ad5a55a910c39c8c2cc1ddcbcfc
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-18 13:31:21 +0200

    [#165442014] Add middleware to the route

--- server/routes/office.routes.js
+++ server/routes/office.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import officeController from '../controllers/office.controller';
+import officeMiddleware from '../middlewares/office.middleware';
 
 const router = express.Router();
 
-router.post('/', officeController.createNewOffice);
+router.post('/', officeMiddleware.areAttibutesValid, officeMiddleware.isNameExist, officeController.createNewOffice);
 router.get('/:id/results', officeController.getOfficesResult);
 router.get('/', officeController.getAllOffices);
 
