4d786c04a3f3b6019a911a8d0673d2197f2b86e3
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-18 15:24:53 +0200

    Rephrase message

--- server/controllers/office.controller.js
+++ server/controllers/office.controller.js
@@ -8,7 +8,7 @@ const officeController = {
           return res.status(500).send({ status: 500, Error: 'Error in getting data' });
         }
         if (getResultQuery.result.length === 0) {
-          return res.status(404).send({ status: 404, Error: 'Offices not found' });
+          return res.status(404).send({ status: 404, Error: 'Result not found' });
         }
         return res.status(200).send({ status: 200, data: getResultQuery.result });
       },
