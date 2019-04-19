6446a2bec5f3bce1847a5f02482ba2a4f1d83d87
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-19 15:13:30 +0200

    [#165442014] Console j token

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -9,6 +9,7 @@ const Auth = {
         }
         try {
             const decoded = await jwt.verify(token, process.env.SECRET);
+            // console.log('J token:',decoded)
             const text = 'SELECT * FROM users WHERE id = $1';
             const { rows } = await dbCon.query(text, [decoded.id]);
             if (!rows[0]) {
