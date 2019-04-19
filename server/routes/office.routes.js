7a06935976db0088270badabdf1c43af42d8c140
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-04-19 15:14:28 +0200

    [#165442014] Add auth check middleware

--- server/routes/office.routes.js
+++ server/routes/office.routes.js
@@ -2,10 +2,15 @@ import express from 'express';
 
 import officeController from '../controllers/office.controller';
 import officeMiddleware from '../middlewares/office.middleware';
+import Auth from '../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/', officeMiddleware.areAttibutesValid, officeMiddleware.isNameExist, officeController.createNewOffice);
+router.post('/', 
+    Auth.verifyToken, 
+    officeMiddleware.areAttibutesValid, 
+    officeMiddleware.isNameExist, 
+    officeController.createNewOffice);
 router.get('/:id/results', officeController.getOfficesResult);
 router.get('/', officeController.getAllOffices);
 
